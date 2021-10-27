e281b093550ac1e5e3c88227150a7d79f7c3f903
Lucas Dario Stocksmeier <lucas@stocksmeier.de>
2021-10-27 07:49:07 +0200

    add tests

--- hello-world/src/main/java/de/lukonjun/helloworld/controller/GreetingsController.java
+++ hello-world/src/main/java/de/lukonjun/helloworld/controller/GreetingsController.java
@@ -3,12 +3,20 @@ package de.lukonjun.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class GreetingsController {
 
     @GetMapping("/greetings")
-    public String greetings(){
+    public String greetings() {
         return "Hello World";
+    }
+
+    @GetMapping("/host")
+    public String host() throws UnknownHostException {
+        return InetAddress.getLocalHost().toString();
     }
 
 }
--- hello-world/src/test/java/de/lukonjun/helloworld/HelloWorldApplicationTests.java
+++ hello-world/src/test/java/de/lukonjun/helloworld/HelloWorldApplicationTests.java
@@ -1,13 +1,32 @@
 package de.lukonjun.helloworld;
 
+import de.lukonjun.helloworld.controller.GreetingsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class HelloWorldApplicationTests {
 
+	@Autowired
+	GreetingsController greetingsController;
+
 	@Test
 	void contextLoads() {
+		assertEquals(greetingsController.greetings(),"Hello World");
+	}
+
+	@Test
+	void anotherTest() {
+		assertTrue(greetingsController.toString().contains("Hello"));
+	}
+
+	@Test
+	void validateNotNull(){
+		assertNotNull(greetingsController.greetings());
 	}
 
 }
